024a6ee7dcf5af54aa61dd3581c97226b9880d2f
swk5276 <swk5276@gmail.com>
2022-09-19 03:13:14 +0900

    3주차 강의 201934011 김성웅

--- 3st/3st.cpp
+++ 3st/3st.cpp
@@ -1,20 +1,98 @@
-﻿// 3st.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
-#include <iostream>
 
-int main()
+#pragma comment(lib, "OpenGL32")
+
+static void error_callback(int error, const char* description)
 {
-    std::cout << "Hello World!\n";
+    fputs(description, stderr);
+}
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+int main(void)
+{
+    GLFWwindow* window;
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+    window = glfwCreateWindow(600,600, "Simple example", NULL, NULL); //window 크기
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float ratio;
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float)height;
+
+        ////90도와 같이 각도로 연산하고 싶으면
+        ////glm의 삼각함수는 radian 형식으로 입력을 받기 때문에 변환해서 넣어주어야한다.
+        //glm::sin(glm::radians(90.0));  
+
+        //glClearColor(.0f, 0.0f, 0.0f, 0.1f);  //배경 검은색
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);  //배경 흰색
+        glClear(GL_COLOR_BUFFER_BIT); 
+        glColor3f(0.0f, 0.0f, 1.0f);  //도형의 색(3은 RGB, f는 float)
+        GLfloat size[5];
+
+        /*반지름이 r 인 원의 중심이 원점에 위치할 때
+        원 위에 있는 임의의 한점은(rcosΘ, rsinΘ)으로 나타낼 수 있다.*/
+
+
+
+
+        double rad = 1;  //반지름 rad
+        glBegin(GL_LINE_STRIP);
+        for (int i = 0; i < 360; i++)
+        { 
+        double angle, x, y;
+        angle = i * 3.141592 / 180;
+        x = rad * cos(angle);
+        y = rad * sin(angle);
+        glVertex2f(x, y); 
+        }
+        glEnd();
+
+        //중점이 원점이 아닌 원(중점 좌표 cx,cy)
+        //double cx = 0.2;
+        //double cy = 0.1;
+        //glVertex2f(x + cx, y + cy);
+
+
+       /*//삼각형 그리기 
+       glBegin(GL_LINE_STRIP);
+        glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+        glVertex3f(0.0f, 1.0f, 0.0f);
+        glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
+        glVertex3f(1.0f, -1.0f, 0.0f);
+        glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
+        glVertex3f(-1.0f, -1.0f, 0.0f);
+        glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+        glVertex3f(0.0f, 1.0f, 0.0f);
+        glEnd();*/
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    exit(EXIT_SUCCESS);
+}
